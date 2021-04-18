345d03b2b6a2ef1149f58a4dbf640c6351584cff
Temilayo1 <65893987+Temilayo1@users.noreply.github.com>
2021-04-18 20:34:24 +0800

    first lab
    1. this is the solutuon to the first lab assignment to check if a number is prime or not
    
    2. its to return true if the sum of  2 numbers is less than 100 and false if otherwise

---
+++ Lab-ass-1.dart
@@ -0,0 +1,25 @@
+void main() {
+  int age = 0;
+
+  switch (age) {
+    case 17:
+      {
+        print("Yes correct!! 17 is a prime number :)");
+      }
+      break;
+
+    case 57:
+      {
+        print(
+            "oh no :( 57 is not a prime number. Look up https://languages.oup.com/google-dictionary-en/ ");
+      }
+      break;
+
+    default:
+      {
+        print(
+            "Hi there, click on https://languages.oup.com/google-dictionary-en/ to read more on prime numbers ");
+      }
+      break;
+  }
+}
